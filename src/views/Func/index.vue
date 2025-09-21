5f36d1bf2d780fb56287d45bfff758e44ebc9e53
Youutaku <85777887+Youutaku@users.noreply.github.com>
2025-09-21 12:00:06 +0800

    Update index.vue

--- src/views/Func/index.vue
+++ src/views/Func/index.vue
@@ -125,7 +125,7 @@ onBeforeUnmount(() => {
           margin-top: 10px;
           font-size: 3.25rem;
           letter-spacing: 2px;
-          font-family: "JetBrainsMono-Regular";
+          font-family: "Inconsolata-Regular";
         }
         @media (min-width: 1201px) and (max-width: 1280px) {
           font-size: 1rem;
